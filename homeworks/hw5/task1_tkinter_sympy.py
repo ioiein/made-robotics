6ffbb87af7fc10af55b6ad4de89d693698a2a1a3
ioiein <71278916+ioiein@users.noreply.github.com>
2021-11-30 15:14:37 +0300

    released A*

--- homeworks/hw5/task1_tkinter_sympy.py
+++ homeworks/hw5/task1_tkinter_sympy.py
@@ -1,10 +1,15 @@
 from tkinter import *
 import math
+import heapq, random
+import time
+import numpy as np
 from sympy import Point, Polygon
 
 '''================= Your classes and methods ================='''
 
+
 # These functions will help you to check collisions with obstacles
+
 
 def rotate(points, angle, center):
     angle = math.radians(angle)
@@ -18,68 +23,112 @@ def rotate(points, angle, center):
         y_old -= cy
         x_new = x_old * cos_val - y_old * sin_val
         y_new = x_old * sin_val + y_old * cos_val
-        new_points.append((x_new+cx, y_new+cy))
+        new_points.append((x_new + cx, y_new + cy))
 
     return new_points
 
-def get_polygon_from_position(position) :
-    x,y,yaw = position
-    points = [(x - 50, y - 100), (x + 50, y - 100), (x + 50, y + 100), (x - 50, y + 100)] 
-    new_points = rotate(points, yaw, (x,y))
+
+def get_polygon_from_position(position):
+    x, y, yaw = position
+    points = [(x - 50, y - 100), (x + 50, y - 100), (x + 50, y + 100), (x - 50, y + 100)]
+    new_points = rotate(points, yaw, (x, y))
     return Polygon(*list(map(Point, new_points)))
 
-def get_polygon_from_obstacle(obstacle) :
-    points = [(obstacle[0], obstacle[1]), (obstacle[2], obstacle[3]), (obstacle[4], obstacle[5]), (obstacle[6], obstacle[7])] 
+
+def get_polygon_from_obstacle(obstacle):
+    points = [(obstacle[0], obstacle[1]), (obstacle[2], obstacle[3]), (obstacle[4], obstacle[5]),
+              (obstacle[6], obstacle[7])]
     return Polygon(*list(map(Point, points)))
 
-def collides(position, obstacle) :
-    return get_polygon_from_position(position).intersection(get_polygon_from_obstacle(obstacle))
-        
 
-class Window():
-        
-    '''================= Your Main Function ================='''
-    
+def collides(position, obstacle):
+    return get_polygon_from_position(position).intersection(get_polygon_from_obstacle(obstacle))
+
+
+class Window:
+    """================= Your Main Function ================="""
+
+    def cost_func(self, position, weights):
+        return 0
+
+    def next_positions(self, position, step_size):
+        return []
+
+    def check_collision(self, position):
+        return True
+
+    def a_star(self, path, step_size, acc, weights, eps):
+        path = path
+        explored = set()
+        frontier = []
+        cost = self.cost_func(path[-1], weights)
+        passed = (len(path) - 1) * step_size
+        state = (cost, passed, path)
+        heapq.heappush(frontier, state)
+
+        while len(frontier) > 0:
+            _, passed, path = heapq.heappop(frontier)
+
+            if self.cost_func(path[-1], weights) < eps:
+                break
+
+            for next_position in self.next_positions(path[-1], step_size):
+                if self.check_collision(next_position):
+                    continue
+
+                if (round(next_position[0], acc),
+                    round(next_position[1], acc),
+                    round(next_position[2], acc)) in explored:
+                    continue
+
+                path_next = path.copy()
+                path_next.append(next_position)
+                explored.add((round(next_position[0], acc),
+                              round(next_position[1], acc), round(next_position[2], acc)))
+                passed += step_size
+                cost = self.cost_func(next_position, weights)
+                state = (passed + cost, passed, path_next)
+                heapq.heappush(frontier, state)
+
+        return path
+
     def go(self, event):
-    
+
         # Write your code here
-                
+
         print("Start position:", self.get_start_position())
-        print("Target position:", self.get_target_position()) 
+        print("Target position:", self.get_target_position())
         print("Obstacles:", self.get_obstacles())
-        
+
         # Example of collision calculation
-        
+
         number_of_collisions = 0
-        for obstacle in self.get_obstacles() :
-            if collides(self.get_start_position(), obstacle) :
+        for obstacle in self.get_obstacles():
+            if collides(self.get_start_position(), obstacle):
                 number_of_collisions += 1
         print("Start position collides with", number_of_collisions, "obstacles")
-        
-        
+
     '''================= Interface Methods ================='''
-    
-    def get_obstacles(self) :
+
+    def get_obstacles(self):
         obstacles = []
         potential_obstacles = self.canvas.find_all()
         for i in potential_obstacles:
-            if (i > 2) :
+            if i > 2:
                 coords = self.canvas.coords(i)
                 if coords:
                     obstacles.append(coords)
         return obstacles
-            
-            
-    def get_start_position(self) :
-        x,y = self.get_center(2) # Purple block has id 2
+
+    def get_start_position(self):
+        x, y = self.get_center(2)  # Purple block has id 2
         yaw = self.get_yaw(2)
-        return x,y,yaw
-    
-    def get_target_position(self) :
-        x,y = self.get_center(1) # Green block has id 1 
+        return x, y, yaw
+
+    def get_target_position(self):
+        x, y = self.get_center(1)  # Green block has id 1
         yaw = self.get_yaw(1)
-        return x,y,yaw 
- 
+        return x, y, yaw
 
     def get_center(self, id_block):
         coords = self.canvas.coords(id_block)
@@ -93,20 +142,20 @@ class Window():
         second_x = 1.0
         second_y = 0.0
         points = self.canvas.coords(id_block)
-        end_x = (points[0] + points[2])/2
-        end_y = (points[1] + points[3])/2
+        end_x = (points[0] + points[2]) / 2
+        end_y = (points[1] + points[3]) / 2
         direction_x = end_x - center_x
         direction_y = end_y - center_y
         length = math.hypot(direction_x, direction_y)
         unit_x = direction_x / length
         unit_y = direction_y / length
-        cos_yaw = unit_x * first_x + unit_y * first_y 
+        cos_yaw = unit_x * first_x + unit_y * first_y
         sign_yaw = unit_x * second_x + unit_y * second_y
-        if (sign_yaw >= 0 ) :
+        if (sign_yaw >= 0):
             return math.acos(cos_yaw)
-        else :
+        else:
             return -math.acos(cos_yaw)
-       
+
     def get_vertices(self, id_block):
         return self.canvas.coords(id_block)
 
@@ -124,8 +173,8 @@ class Window():
             y_old -= cy
             x_new = x_old * cos_val - y_old * sin_val
             y_new = x_old * sin_val + y_old * cos_val
-            new_points.append(x_new+cx)
-            new_points.append(y_new+cy)
+            new_points.append(x_new + cx)
+            new_points.append(y_new + cy)
 
         return new_points
 
@@ -221,8 +270,10 @@ class Window():
         cat2 = self.distance(wx, wy, block[4], block[5])
         hyp = self.distance(x, y, wx, wy)
 
-        if wx - x > 0: angle = math.acos((cat1**2 + cat2**2 - hyp**2) / (2 * cat1 * cat2))
-        elif wx - x < 0: angle = -math.acos((cat1**2 + cat2**2 - hyp**2) / (2 * cat1 * cat2))
+        if wx - x > 0:
+            angle = math.acos((cat1 ** 2 + cat2 ** 2 - hyp ** 2) / (2 * cat1 * cat2))
+        elif wx - x < 0:
+            angle = -math.acos((cat1 ** 2 + cat2 ** 2 - hyp ** 2) / (2 * cat1 * cat2))
 
         new_block = self.rotate([block[0:2], block[2:4], block[4:6], block[6:8]], angle, center)
         self.canvas.coords(id, new_block)
@@ -234,7 +285,7 @@ class Window():
             if widget.coords(i) == []:
                 break
             if self.in_rect([event.x, event.y], widget.coords(i)):
-                widget.coords(i, [0,0])
+                widget.coords(i, [0, 0])
                 break
 
     def create_block(self, event):
@@ -315,23 +366,24 @@ class Window():
 
         self.create_button_create()
         self.create_button_go()
-        self.create_green_block(self.width/2)
-        self.create_purple_block(self.width/2, self.height)
+        self.create_green_block(self.width / 2)
+        self.create_purple_block(self.width / 2, self.height)
 
         root.bind("<Delete>", self.delete_block)
 
         root.mainloop()
-        
+
     def __init__(self):
         self.root = Tk()
         self.root.title("")
-        self.width  = self.root.winfo_screenwidth()
+        self.width = self.root.winfo_screenwidth()
         self.height = self.root.winfo_screenheight()
         self.root.geometry(f'{self.width}x{self.height}')
         self.canvas = Canvas(self.root, bg="#777777", height=self.height, width=self.width)
         self.canvas.pack()
         # self.points = [0, 500, 500/2, 0, 500, 500]
-    
+
+
 if __name__ == "__main__":
     run = Window()
     run.run()
